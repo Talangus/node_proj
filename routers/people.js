7bd9be24573c426ecf835e791d6dcb0789ed0cbb
tsipib <tzipibarel4@gmail.com>
2022-06-01 19:22:09 +0300

    tsipi comments

--- routers/people.js
+++ routers/people.js
@@ -13,6 +13,7 @@ peopleRouter.get('/', (req, res) => {
 });
 
 peopleRouter.post('/', (req, res) => {
+  /* check all fildes exists and not null (extra values?) */
   const newPerson = new PersonData(req.body.name, req.body.emails, req.body.favoriteProgrammingLanguage); 
   db.insertPersonData(newPerson)
     .then(res.status(201).send('Person created successfully'))
@@ -20,27 +21,44 @@ peopleRouter.post('/', (req, res) => {
  });
 
 peopleRouter.get('/:id', (req, res) => {
-  console.log(req.params.id);
     db.getPersonDetails(req.params.id)
       .then(data => res.send(data));
       //.catch(res.send("A person with the id does not exists.")) //(throw error if doesn't exist)
 });
 
 peopleRouter.patch('/:id', (req, res) => {
+  /* check id exists */
+  /* if all fildes are empty - return 200 */
     db.updatePersonDetails(req.params.id, req.body)
       .then(res.send('Person updated successfully. Response body contains updated data.')).catch(err => console.log(err));
 }); //complete patch 
 
 
-peopleRouter.delete('/:id', (req, res) => {});// db.removePersondetails(id) throws error if dosent exist
+peopleRouter.delete('/:id', (req, res) => {
+  /* check id exists */
+  db.removePersondetails(req.params.id)
+    .then()
+    .catch();
+}); //throws error if dosent exist
 
-peopleRouter.get('/:id/tasks', (req, res) => {}); // 
+peopleRouter.get('/:id/tasks', (req, res) => {
+  /* check id exists */
+  /* add to url optional status and check if it's Active/Done only */
+}); // 
   //db.getPersonTaskdetails(id, status (optional)) /returns tasks of person
 
 
 peopleRouter.post('/:id/tasks', (req, res) => {});
-  
+  /* check id exists */
+  /* is status is invalid - error ? ask michael */
+  /* check all fildes exists and not null (extra values?) , different tasks - different fildes */
   //db.insertTaskData (id, TaskData) //need to add to task table and update person table
 
 
 module.exports = peopleRouter;
+
+
+/* TODO:
+errors catch 
+emails vs. email - ask michael
+ */
