86cdc9be9d70436dd18ca06060b4402c378b639c
tsipib <tzipibarel4@gmail.com>
2022-06-04 17:11:03 +0300

    add people api functions- delete, get tasks, post tasks

--- routers/people.js
+++ routers/people.js
@@ -22,8 +22,7 @@ peopleRouter.post('/', (req, res) => {
 
 peopleRouter.get('/:id', (req, res) => {
     db.getPersonDetails(req.params.id)
-      .then(data => res.send(data));
-      //.catch(res.send("A person with the id does not exists.")) //(throw error if doesn't exist)
+      .then(data => data ? res.send(data) : res.send("A person with the id does not exists.")); //(throw error if doesn't exist)
 });
 
 peopleRouter.patch('/:id', (req, res) => {
@@ -37,23 +36,66 @@ peopleRouter.patch('/:id', (req, res) => {
 peopleRouter.delete('/:id', (req, res) => {
   /* check id exists */
   db.removePersondetails(req.params.id)
-    .then()
-    .catch();
+    .then(data => data ? res.send('Person removed successfully.') : 
+                          res.status(404).send('A person with the id '+req.params.id+' does not exist.'))
+    .catch(()=> res.send('A person with the id '+req.params.id+' does not exist.'));
 }); //throws error if dosent exist
 
 peopleRouter.get('/:id/tasks', (req, res) => {
   /* check id exists */
   /* add to url optional status and check if it's Active/Done only */
-}); // 
-  //db.getPersonTaskdetails(id, status (optional)) /returns tasks of person
+  /* returns tasks of person */
+  db.getPersonDetails(req.params.id)
+    .then(ans => { 
+      if(ans) {
+        const status = req.query ? req.query.status.toLowerCase() : undefined;
+        if(status != "active" && status != "done" && status != undefined)
+          res.status(404).send('Invalid Status.');
+        else{
+          db.getPersonTaskdetails(req.params.id, status)
+            .then(data => res.send(data))
+            .catch(err => console.log(err));
+        }
+      }
+      else {
+        res.send('A person with the id '+req.params.id+' does not exist.')
+      }});
+  });
 
-
-peopleRouter.post('/:id/tasks', (req, res) => {});
+peopleRouter.post('/:id/tasks', (req, res) => {
   /* check id exists */
   /* is status is invalid - error ? ask michael */
   /* check all fildes exists and not null (extra values?) , different tasks - different fildes */
-  //db.insertTaskData (id, TaskData) //need to add to task table and update person table
-
+  /* need to add to task table and update person table */
+  db.getPersonDetails(req.params.id)
+    .then(ans => { 
+      if(ans) {
+        const type = req.body.type;
+        if (type === 'Chore'){
+          if (req.body.description == undefined || req.body.size == undefined ||
+              (req.body.status != 'Active' && req.body.status != 'Done' && req.body.status != undefined))
+            res.status(404).send('Invalid task fields.');
+          else {
+            db.insertTaskData(req.params.id, req.body)
+            .then(data => res.send('Task created and assigned successfully'))
+            .catch(err => console.log(err));
+          }
+        }
+        if (type === 'HomeWork'){
+          if (req.body.course == undefined || req.body.details == undefined || req.body.dueDate == undefined
+              (req.body.status != 'Active' && req.body.status != 'Done' && req.body.status != undefined))
+            res.status(404).send('Invalid task fields.');
+          else {
+            db.insertTaskData(req.params.id, req.body)
+            .then(data => res.send('Task created and assigned successfully'))
+            .catch(err => console.log(err));
+          }
+        }
+      }
+      else {
+        res.send('A person with the id '+req.params.id+' does not exist.')
+      }});
+});
 
 module.exports = peopleRouter;
 
